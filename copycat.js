3fd6bdd6961e29f05fb22ea72fb9ff0e3051ccca
LaurentSch <48076819+LaurentSch@users.noreply.github.com>
2023-04-28 01:38:57 +0200

    Move solutions to riddles from results page to below respective riddle
    Remove back button.
    Add sumbit button.
    
    The user now has to first submit their answer with the 'submit'
    button, and then immediately gets show the solution to it.

--- copycat.js
+++ copycat.js
@@ -8,7 +8,7 @@ function ID(elementId) {
 // Tracks what puzzle the user is on
 let currentPuzzle = 1;
 // array with the questions
-let questions = new Array(4)
+let questions = new Array(4);
 questions[0] = "A father and son have a car accident and are both badly hurt. They are both taken to separate hospitals. When the boy is taken in for an operation, the surgeon (doctor) says 'I can not do the surgery because this is my son'. \n How is this possible?";
 questions[1] = "A classic greek riddle: \nWhat goes on four legs in the morning, \ntwo legs in the afternoon, and \nthree legs in the evening?";
 questions[2] = "What always runs but never walks. \nOften murmurs, never talks. \nHas a bed but never sleeps. \nAn open mouth that never eats. \nWhat is it?";
@@ -19,6 +19,29 @@ solutions[0] = new Array("mom", "mother", "mum");
 solutions[1] = new Array("human", "person", "man");
 solutions[2] = new Array("river", "stream");
 solutions[3] = new Array("time")
+// The full explanations for the user
+let explanations = new Array(4);
+explanations[0] = "The answer is the <b>Mother:</b><hr>"
+					+ "The difficulty in this riddle was that for some non-english speakers (like german and luxembourgish people), there is a masculine and feminine form of doctor.<br>"
+					+ "And the masculine form sounds very close to 'doctor'. So it is easy to assume that the doctor has to be masculine."
+explanations[1] = "The answer is a <b>Human:</b><hr>"
+					+ "In this riddle the time of the day is a metaphor for the stages of life in a human. In greek myth, a sphinx asked Oedipus this riddle.<br>"
+                    + "He answered the riddle correctly: 'The answer is `man`. A man crawls on all fours in the morning of his life, he walks on two feet in the midday of his life, and he uses a cane for extra support when he is old.'<hr>"
+                    + "Note that, while inspired from it, the greek sphinx is not the same as the egyptian sphinx."
+explanations[2] = "The answer to the riddle is a <b>River:</b><hr>"
+                    + "<i>What always runs but never walks.</i>: A river is constantly flowing, or running, but it never walks since it is a body of water.<hr>"
+                    + "<i>Often murmurs, never talks</i>: The sound of a river is often described as a murmur, as it makes a continuous low, soft sound."
+                    + "However, a river doesn't talk because it doesn't have a voice or the ability to communicate.<hr>"
+                    + "<i>Has a bed but never sleeps</i>: A river has a bed, which is the channel it flows through. However, a river doesn't sleep because it's always flowing.<hr>"
+                    + "<i>An open mouth that never eats	</i>: A river has a mouth, which is the place where it empties into a larger body of water, such as a lake or ocean."
+                    + "This mouth is typically wide open, but a river never eats because it's not alive and doesn't have a digestive system.<hr>"
+explanations[3] = "The answer is <b>Time:</b><hr>"
+                    + "<i>I'm a thief that cannot be caught</i>: This line suggests that the answer is something that takes something away, but is not tangible.<hr>"
+                    + "<i>Stealing moments that cannot be bought</i>: Since it is impossible to regain lost time.<hr>"
+                    + "<i>I'm the reason for the rising sun</i>: This line suggests that the answer is something that is closely related to the passage of time, and specifically to the cycle of day and night.\n<hr>"
+                    + "<i>And the setting of the day when it's done</i>: This line completes the previous line and reinforces the idea that the answer is related to the passage of time and the cycle of day and night.<hr>"
+
+
 // save the users answers
 let answers = new Array(4).fill(null);
 // set timer in seconds
@@ -54,28 +77,37 @@ function updatePuzzle(currentPuzzle) {
 			ID("puzzle-img").src = './data/hospital.png';
 			ID("puzzle-text").innerText = questions[0];
 			ID("answer").value = answers[currentPuzzle]
-			ID("back-btn").disabled = true;
+			ID("next-btn").style.display = 'none';
+			ID("hidden-solution").style.display = 'none';
+			ID("user-input").style.display = "block"
+			ID("submit-btn").style.display = 'inline-block';
 			break;
 		case 2:
 			ID("puzzle-number").innerText = 'El classico.\n Puzzle 2/4:';
 			ID("puzzle-img").src = './data/sphinx.png';
 			ID("puzzle-text").innerText = questions[1];
-			ID("back-btn").style.display = 'inline-block';
-            ID("back-btn").disabled = false;
+			ID("next-btn").style.display = 'none';
+			ID("hidden-solution").style.display = 'none';
+			ID("user-input").style.display = "block"
+			ID("submit-btn").style.display = 'inline-block';
 			break;
 		case 3:
 			ID("puzzle-number").innerText  = 'A bit more difficult.\n Puzzle 3/4:';
 			ID("puzzle-img").src = './data/nguruvilu.png';
 			ID("puzzle-text").innerText = questions[2];
-			ID("next-btn").disabled = false;
-			ID("finish-btn").style.display = 'none';
+			ID("next-btn").style.display = 'none';
+			ID("hidden-solution").style.display = 'none';
+			ID("user-input").style.display = "block"
+			ID("submit-btn").style.display = 'inline-block';
 			break;
 		case 4:
 			ID("puzzle-number").innerText  = 'Almost there.\n Puzzle 4/4:';
 			ID("puzzle-img").src = './data/fox_thief.png';
 			ID("puzzle-text").innerText = questions[3];
-			ID("next-btn").disabled = true;
-			ID("finish-btn").style.display = 'inline-block';
+			ID("next-btn").style.display = 'none';
+			ID("hidden-solution").style.display = 'none';
+			ID("user-input").style.display = "block"
+			ID("submit-btn").style.display = 'inline-block';
 			break;
 		default:
 			break;
@@ -85,10 +117,10 @@ function updatePuzzle(currentPuzzle) {
 // Checks if there is a next puzzle then calls saveSolution and updatePuzzle
 function goNext() {
 	if (currentPuzzle < 5) {
-		saveSolution()
+		// saveSolution()
 		currentPuzzle++;
 		updatePuzzle(currentPuzzle);
-		ID("answer").value = answers[currentPuzzle - 1]
+		// ID("answer").value = answers[currentPuzzle - 1]
         // ID("puzzle-number").innerText  = 'Puzzle ' +  currentPuzzle + '/5';
 	}
     //console.log("aiogfuhapiefgba8izwegf8iewgfaiwezugfapö<iewzfgbapöiugfpöaeiugf")
@@ -105,6 +137,20 @@ function goBack() {
         //update header
         // ID("puzzle-number").innerText  = 'Puzzle ' +  currentPuzzle + '/5';
     }
+}
+
+// Submit user answer, reveal solution, hide 'submit' button and reveal 'next' button
+function submitAnswer() {
+	answers[currentPuzzle - 1] = ID("answer").value;
+	ID("hidden-solution").style.display = 'block';
+	ID("puzzle-solution").innerHTML = explanations[currentPuzzle - 1];
+	ID("submit-btn").style.display = "none";
+	ID("next-btn").style.display = "inline-block"
+	ID("user-input").style.display = "none"
+	if (currentPuzzle == 4) {
+		ID("finish-btn").style.display = 'inline-block';
+		ID("next-btn").style.display = "none"
+	}
 }
 
 // Saves the selected radio button into the solution array by its id.
